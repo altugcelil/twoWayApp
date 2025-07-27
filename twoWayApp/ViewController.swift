7e814754b6a02c9913a82465b2a1b0f0fd0d2bf2
Altug Celil Koc (Berkut Teknoloji) <AltugCelil.Koc@ext.d-teknoloji.com.tr>
2025-07-28 01:26:42 +0300

    first init

---
+++ twoWayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twoWayApp
+//
+//  Created by Altug Celil Koc (Berkut Teknoloji) on 28.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
